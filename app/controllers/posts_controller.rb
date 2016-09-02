eb89d25589e0488a4e8ecd9dc254e22ea30ebf46
Marco Martinez & Peter Duke & Wolfgang A Criollo <pair+marcomartinez10.p-duke.savagesnake@devbootcamp.com>
2016-09-02 17:31:28 -0400

    routes and devise setup

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  # before_filter :authenticate_user!
+
+  def index
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+
+
+  # resources :topics, :only=> [:index]
+  # resources :responses
+  # resources :posts
+
+  resources :topics, :only=> [:index] do
+    resources :posts do
+      resources :responses
+    end
+  end
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
+  # authenticated :user do
+  #   root to: 'topics#index', as: :authenticated_root
+  # end
+
+  match 'home/index' => "home#index", as: :user_root, via: [:get]
 end

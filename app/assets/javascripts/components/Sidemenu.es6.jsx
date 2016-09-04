1e237ec3026f7ca109ce9a1a34bc67a81d94a575
Wolfgang A Criollo <wolfplisken@gmail.com>
2016-09-04 10:33:52 -0400

    siedmenu bar updated

--- app/assets/javascripts/components/Sidemenu.es6.jsx
+++ app/assets/javascripts/components/Sidemenu.es6.jsx
@@ -5,7 +5,7 @@ class Sidemenu extends React.Component {
           <div className="left_col scroll-view">
 
             <div className="navbar nav_title" style={{"border":"0"}}>
-              <a href="index.html" className="site_title"><i className="fa fa-paw"></i> <span>Gentellela Alela!</span></a>
+              <a href="index.html" className="site_title"><i className="fa fa-paw"></i> <span>Devconnect</span></a>
             </div>
             <div className="clearfix"></div>
 

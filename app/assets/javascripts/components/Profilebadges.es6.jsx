d1d1f643fb323623fb7d942b3df687f9190d33b6
Wolfgang A Criollo <email": null>
2016-09-05 19:30:55 -0400

    set up the user badges component

---
+++ app/assets/javascripts/components/Profilebadges.es6.jsx
@@ -0,0 +1,24 @@
+class Profilebadges extends React.Component{
+  render(){
+    return(
+      <div className="col-md-8 col-sm-8 col-xs-12">
+          <div className="x_panel">
+            <div className="x_title">
+              <h2>User Badges</h2>
+              <div className="clearfix"></div>
+            </div>
+            <div className="x_content">
+              <div className="dashboard-widget-content">
+
+                <ul className="list-unstyled  widget">
+                  <li>
+                   <img width="300"  src="http://www.pixelhustler.uk/wp-content/uploads/2013/04/dev_page_badges.jpg"/>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+  }
+}

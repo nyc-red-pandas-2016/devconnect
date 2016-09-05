9cc4ef11fcbedfd4d7d6a9f4cf6130796dab012a
Marco Martinez & Peter Duke & Wolfgang A Criollo <pair+marcomartinez10.p-duke.savagesnake@devbootcamp.com>
2016-09-05 18:39:57 -0400

    format news tab section on homepage

--- app/assets/javascripts/components/HomeForumNews.es6.jsx
+++ app/assets/javascripts/components/HomeForumNews.es6.jsx
@@ -6,60 +6,13 @@ class HomeForumNews extends React.Component {
           <li>
             <img src="images/img.jpg" className="avatar" alt="Avatar"/>
             <div className="message_date">
-              <h3 className="date text-info">24</h3>
-              <p className="month">May</p>
+              <h3 className="date text-error">25</h3>
+              <p className="month">August</p>
             </div>
             <div className="message_wrapper">
-              <h4 className="heading">Desmond Davison</h4>
-              <blockquote className="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-              <br />
-              <p className="url">
-                <span className="fs1 text-info" aria-hidden="true" data-icon=""></span>
-                <a href="#"><i className="fa fa-paperclip"></i> User Acceptance Test.doc </a>
-              </p>
-            </div>
-          </li>
-          <li>
-            <img src="images/img.jpg" className="avatar" alt="Avatar"/>
-            <div className="message_date">
-              <h3 className="date text-error">21</h3>
-              <p className="month">May</p>
-            </div>
-            <div className="message_wrapper">
-              <h4 className="heading">Brian Michaels</h4>
-              <blockquote className="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-              <br />
-              <p className="url">
-                <span className="fs1" aria-hidden="true" data-icon=""></span>
-                <a href="#" data-original-title="">Download</a>
-              </p>
-            </div>
-          </li>
-          <li>
-            <img src="images/img.jpg" className="avatar" alt="Avatar"/>
-            <div className="message_date">
-              <h3 className="date text-info">24</h3>
-              <p className="month">May</p>
-            </div>
-            <div className="message_wrapper">
-              <h4 className="heading">Desmond Davison</h4>
-              <blockquote className="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
-              <br />
-              <p className="url">
-                <span className="fs1 text-info" aria-hidden="true" data-icon=""></span>
-                <a href="#"><i className="fa fa-paperclip"></i> User Acceptance Test.doc </a>
-              </p>
-            </div>
-          </li>
-          <li>
-            <img src="images/img.jpg" className="avatar" alt="Avatar"/>
-            <div className="message_date">
-              <h3 className="date text-error">21</h3>
-              <p className="month">May</p>
-            </div>
-            <div className="message_wrapper">
-              <h4 className="heading">Brian Michaels</h4>
-              <blockquote className="message">Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth.</blockquote>
+              <h4 className="heading">Dev Bootcamp</h4>
+              <blockquote className="message">A Day in the Life of a Dev Bootcamp student</blockquote><br />
+              <blockquote className="message">A day at Dev Bootcamp is far different than any school or educational institution you’ve experienced. Some describe it as organized chaos, others as having a summer camp feel. At Dev Bootcamp we like to think of the inclusive learning environment at each of our campuses as a simulation for what it’s like to...</blockquote>
               <br />
               <p className="url">
                 <span className="fs1" aria-hidden="true" data-icon=""></span>

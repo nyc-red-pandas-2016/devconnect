e31deef36e7cd53e75f4f986cddc06994cfaf3a0
Elizabeth Lefever <email": null>
2016-09-07 15:39:53 -0400

    added news to home page

--- app/assets/javascripts/components/HomeMain.es6.jsx
+++ app/assets/javascripts/components/HomeMain.es6.jsx
@@ -4,11 +4,21 @@ class HomeMain extends React.Component {
     super();
     this.state = {
       topTrending: [],
-      mostRecent: []
+      mostRecent: [],
+      news: []
     }
   }
 
   componentDidMount() {
+    fetch('http://devconnect-admin.herokuapp.com/news')
+    .then((response) => {
+      return response.json()
+    }).then((response) => {
+      this.setState({news: response})
+    }).catch(function(err) {
+      console.log(err);
+    });
+
     fetch('/home/index/forumupdate')
     .then((response) => {
       return response.json()
@@ -24,7 +34,7 @@ class HomeMain extends React.Component {
     return(
       <div>
         <h4>Welcome to DevConnect!</h4>
-          <HomeForum trending={this.state.topTrending} recent={this.state.mostRecent}/>
+          <HomeForum trending={this.state.topTrending} recent={this.state.mostRecent} news={this.state.news}/>
           <p>HomeMentor</p>
       </div>
     );

1274536d37a9aab3c2ec9937df4834af8250f8aa
Marco Martinez <email": null>
2016-09-07 11:51:23 -0400

    update user ready

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,7 +17,6 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
   def create
-    # binding.pry
     cohort = Cohort.find_by(name: params[:user][:cohort_name], location: params[:user][:cohort_location], year: params[:user][:cohort_year])
     if cohort != nil
       user = User.new(sign_up_params)
@@ -28,7 +27,6 @@ class RegistrationsController < Devise::RegistrationsController
       return
     end
       if user.save
-        binding.pry
         redirect_to '/home/index'
       else
         flash[:error] = user.errors.full_messages
@@ -38,15 +36,20 @@ class RegistrationsController < Devise::RegistrationsController
 
   def update
     if skills_info_params
-      current_user.skills << Skill.new(skills_info_params)
+
+      skill = Skill.find_by(name: skills_info_params[:name])
+      Endorsement.create(endorsed_id: current_user.id, endorser_id: current_user.id, skill_id: skill.id)
       if current_user.save
         redirect_to '/users/edit'
       else
         flash[:error] = current_user.errors.full_messages
         redirect_to '/users/edit'
       end
+
     else
+
       if contact_info_params
+
           current_user.forms_of_contact << ContactInfo.new(contact_info_params)
           if current_user.save
             redirect_to '/users/edit'
@@ -54,16 +57,21 @@ class RegistrationsController < Devise::RegistrationsController
             flash[:error] = current_user.errors.full_messages
             redirect_to '/users/edit'
           end
+
       else
 
         update_resource(current_user, account_update_params)
-        # binding.pry
         if current_user.save
-          redirect_to '/home/index'
+          if (account_update_params[:password] != "" &&   account_update_params[:password_confirmation] != "")
+            redirect_to "/"
+          else
+            redirect_to "/users/#{current_user.id}/show"
+          end
         else
           flash[:error] = current_user.errors.full_messages
           redirect_to '/users/edit'
         end
+
       end
     end
   end
@@ -71,26 +79,28 @@ class RegistrationsController < Devise::RegistrationsController
     def edit
       @contact_info = ContactInfo.new
       @skillsnames = []
+      userskillsnames = []
+      current_user.skills.each do |userskill|
+        userskillsnames << userskill.name
+      end
       Skill.all.each do |skill|
         @skillsnames << skill.name
+        @skillsnames = @skillsnames - userskillsnames
       end
-      # params.require(:contact_info).permit(:contact_type, :contact_link, :user_id)
-
     end
 
     def destroy
-#       if request.xhr?
-#   response
-# else
-  item = current_user.forms_of_contact.find(contact_delete_params[:id])
-  item.destroy
-  # binding.pry
-  redirect_to '/users/edit'
-  return
-# end
-
-
-
+      if contact_delete_params
+        item = current_user.forms_of_contact.find(contact_delete_params[:id])
+        item.destroy
+        redirect_to '/users/edit'
+        return
+      elsif skill_delete_params
+        item = current_user.endorsements.find_by(skill_id: skill_delete_params[:id])
+        item.destroy
+        redirect_to '/users/edit'
+        return
+      end
     end
 
 
@@ -118,13 +128,19 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-
-    params.require(:user).permit(:id, :location, :status, :place_of_work, :position, :goals, :experience, :bio, :email, :password, :password_confirmation, :current_password, forms_of_contact_attributes: [:contact_type, :contact_link,:user_id, :id])
-
+    params.require(:user).permit(:id, :location, :status, :place_of_work, :position, :goals, :experience, :bio, :email, :is_mentor, :password, :password_confirmation, :current_password, forms_of_contact_attributes: [:contact_type, :contact_link,:user_id, :id])
   end
 
   def contact_delete_params
-    params.require(:contact_info).permit(:id)
+    if params[:contact_info]
+      params.require(:contact_info).permit(:id)
+    end
+  end
+
+  def skill_delete_params
+    if params[:skills]
+      params.require(:skills).permit(:id)
+    end
   end
 
 end

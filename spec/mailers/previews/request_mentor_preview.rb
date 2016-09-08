50203628b5161861da44b7f2fc31b80e4326a59c
Marco Martinez & Peter Duke & Wolfgang A Criollo <pair+marcomartinez10.p-duke.savagesnake@devbootcamp.com>
2016-09-08 13:09:03 -0400

    email notifications feature working

--- spec/mailers/previews/request_mentor_preview.rb
+++ spec/mailers/previews/request_mentor_preview.rb
@@ -1,6 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/request_mentor
 class RequestMentorPreview < ActionMailer::Preview
   def sample_mail_preview
-    RequestMentorMailer.sample_email(User.find(6), User.find(6))
+    RequestMentorMailer.mentor_email(User.find(1), User.find(1))
+    # RequestMentorMailer.user_email(User.find(1), User.find(1))
   end
 end

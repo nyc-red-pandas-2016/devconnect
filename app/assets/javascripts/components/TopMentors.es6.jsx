8b0e2470d41ec36d5fdb3459250dc9684adf9582
Wolfgang A Criollo <wolfplisken@gmail.com>
2016-09-08 19:14:15 -0400

    style the top mentor section on homepage

--- app/assets/javascripts/components/TopMentors.es6.jsx
+++ app/assets/javascripts/components/TopMentors.es6.jsx
@@ -21,17 +21,25 @@ class TopMentors extends React.Component {
     return(
      <div >
      <div className="top-mentors row">
+     <div className="clearfix"></div>
       <h2>Top Mentors</h2>
       <hr/>
            {this.state.topMentors.map((mentor,index)=>{
              return(
-               <a  key={index} href={`/users/${mentor.id}/show`}>
-               <div className="mentor-card col-xs-6 col-md-4">
+               <div className="col-md-3 col-sm-3 col-xs-12 profile_details"  key={index}>
+                 <div className="well profile_view">
+                   <div className="col-sm-12">
+                   <h4 className="brief">{mentor.location}</h4>
+               <div className="mentor-card">
                 <img  className="mentor-card-image" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" alt="profile/img" />
-                <span className="mentor-name"> {mentor.first_name} {mentor.last_name}</span>
-                <span className="mentor-location">{mentor.location}</span>
+                <p className="mentor-name"> {mentor.first_name} {mentor.last_name}</p>
                </div>
-             </a>
+              </div>
+              <div className="col-xs-12 bottom text-center">
+              <a  className="btn btn-success btn-xs" href={`/users/${mentor.id}/show`}>visit profile</a>
+              </div>
+              </div>
+              </div>
            )
            })
            }

8f381f6fe0839712f4b815aefd074e39c1dfe400
Marco Martinez & Peter Duke & Wolfgang A Criollo <pair+marcomartinez10.p-duke.savagesnake@devbootcamp.com>
2016-09-08 14:57:52 -0400

    fixed prod config typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
     bucket: ENV['S3_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    s3_region: ENV['AWS_REGION']
+    s3_region: ENV['AWS_REGION'],
     s3_host_name: 's3-us-west-2.amazonaws.com',
     s3_protocol: 'https'
   }
